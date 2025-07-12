f6662ff873f5d96da1c07a7e73b793b9c0a53362
Ashish Bamba <ashishbamba@Ashishs-MBP.home>
2025-07-12 01:21:56 -0700

    Update schema

--- schema.sql
+++ schema.sql
@@ -12,25 +12,3 @@ CREATE TABLE predictions (
   r_debt_savings FLOAT,
   score FLOAT
 );
-
-
-
--- Retrieve top 5 recent predictions
-SELECT * FROM predictions
-ORDER BY timestamp DESC
-LIMIT 5;
-
--- Insert a new prediction
-INSERT INTO predictions (income, debt, savings, expenditure, score)
-VALUES (50000, 10000, 2000, 15000, 720);
-
--- Check average credit score by income bracket
-SELECT
-  CASE
-    WHEN income < 30000 THEN 'Low Income'
-    WHEN income BETWEEN 30000 AND 70000 THEN 'Mid Income'
-    ELSE 'High Income'
-  END AS income_bracket,
-  AVG(score) AS avg_score
-FROM predictions
-GROUP BY income_bracket;
